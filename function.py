778c6ca4014e6e8e69965de064539b583edfb65d
Chris18750 <73875226+Chris18750@users.noreply.github.com>
2022-04-30 15:25:34 -0500

    Update function.py

--- function.py
+++ function.py
@@ -4,15 +4,35 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pylab as plt
 
-def method_0:
-  
-def method_1:
-  
-def method_2:
+def MultinomialNB():
+  from sklearn.naive_bayes import MultinomialNB
+  from sklearn.feature_extraction.text import CountVectorizer
 
-def method_3:
+  #Import scikit-learn metrics module for accuracy calculation
+  from sklearn import metrics
 
-def method_4:
+  (x_train, y_train), (x_test, y_test) =  keras.datasets.imdb.load_data()
+
+  x_train = keras.preprocessing.sequence.pad_sequences(x_train, maxlen=256, padding='post', truncating='post', value=0)
+  x_test = keras.preprocessing.sequence.pad_sequences(x_test, maxlen=256, padding='post', truncating='post', value=0)
+
+  model = MultinomialNB()
+  clf = model.fit(x_train, y_train)
+
+  predicted= clf.predict(x_test)
+
+  accuracy = metrics.accuracy_score(y_test, predicted)
+  #print("MultinomialNB Accuracy:", accuracy)
+  return accuracy
+  
+  
+def method_1():
+  
+def method_2():
+
+def method_3():
+
+def method_4():
   
 (x_train, y_train), (x_test, y_test) =  keras.datasets.imdb.load_data(num_words=None, maxlen=None)
 words_index = keras.datasets.imdb.get_word_index() #Gets word index where indexes are words
