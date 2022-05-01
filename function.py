7eb0515e693c39947d4e4af21cd035b75d14c169
ZachRoss1 <99152106+ZachRoss1@users.noreply.github.com>
2022-05-01 13:37:48 -0500

    Update function.py

--- function.py
+++ function.py
@@ -3,7 +3,8 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 import matplotlib.pylab as plt
-
+from sklearn.naive_bayes import MultinomialNB, GaussianNB, ComplementNB, BernoulliNB
+from sklearn import metrics
 #Resources
 #https://docs.w3cub.com/scikit_learn/modules/generated/sklearn.naive_bayes.multinomialnb
 #https://www.analyticsvidhya.com/blog/2021/07/performing-sentiment-analysis-with-naive-bayes-classifier/
@@ -33,11 +34,80 @@ def MultinomialNB():
 def method_1():
 
 #GuassianNB
-def method_2():
+#https://www.analyticsvidhya.com/blog/2021/07/performing-sentiment-analysis-with-naive-bayes-classifier/
+def Guassian():
+  (x_train, y_train), (x_test, y_test) =  keras.datasets.imdb.load_data(num_words=10000)
+  data = np.concatenate((x_train, x_test), axis=0)
+  targets = np.concatenate((y_train, y_test), axis=0)
 
-def method_3():
+  #Training set gets first 20000 examples
+  x_train = data[:20000]
+  y_train = targets[:20000]
+  #Testing gets last 20000 examples
+  x_test = data[20000:]
+  y_test = targets[20000:]
+  #Standardizing review lengths by padding to maxlen 3000
+  x_train = tf.keras.preprocessing.sequence.pad_sequences(x_train, maxlen=3000, padding='post', truncating='post', value=0)
+  x_test = tf.keras.preprocessing.sequence.pad_sequences(x_test, maxlen=3000, padding='post', truncating='post', value=0)
+    
+  model = GaussianNB()
+  
+  clf = model.fit(x_train, y_train)
+  
+  predicted= clf.predict(x_test)
 
-def method_4():
+  accuracy = metrics.accuracy_score(y_test, predicted)
+  print("GaussianNB Accuracy:", accuracy)
+#ComplementNB
+#https://www.analyticsvidhya.com/blog/2021/07/performing-sentiment-analysis-with-naive-bayes-classifier/
+def Complement():
+  (x_train, y_train), (x_test, y_test) =  keras.datasets.imdb.load_data(num_words=10000)
+  data = np.concatenate((x_train, x_test), axis=0)
+  targets = np.concatenate((y_train, y_test), axis=0)
+
+  #Training set gets first 20000 examples
+  x_train = data[:20000]
+  y_train = targets[:20000]
+  #Testing gets last 20000 examples
+  x_test = data[20000:]
+  y_test = targets[20000:]
+  #Standardizing review lengths by padding to maxlen 3000
+  x_train = tf.keras.preprocessing.sequence.pad_sequences(x_train, maxlen=3000, padding='post', truncating='post', value=0)
+  x_test = tf.keras.preprocessing.sequence.pad_sequences(x_test, maxlen=3000, padding='post', truncating='post', value=0)
+    
+  model = ComplementNB()
+  
+  clf = model.fit(x_train, y_train)
+  
+  predicted= clf.predict(x_test)
+
+  accuracy = metrics.accuracy_score(y_test, predicted)
+  print("ComplementNB Accuracy:", accuracy)
+#BernoulliNB
+#https://www.analyticsvidhya.com/blog/2021/07/performing-sentiment-analysis-with-naive-bayes-classifier/
+def Bernoulli():
+  (x_train, y_train), (x_test, y_test) =  keras.datasets.imdb.load_data(num_words=10000)
+  data = np.concatenate((x_train, x_test), axis=0)
+  targets = np.concatenate((y_train, y_test), axis=0)
+
+  #Training set gets first 20000 examples
+  x_train = data[:20000]
+  y_train = targets[:20000]
+  #Testing gets last 20000 examples
+  x_test = data[20000:]
+  y_test = targets[20000:]
+  #Standardizing review lengths by padding to maxlen 3000
+  x_train = tf.keras.preprocessing.sequence.pad_sequences(x_train, maxlen=3000, padding='post', truncating='post', value=0)
+  x_test = tf.keras.preprocessing.sequence.pad_sequences(x_test, maxlen=3000, padding='post', truncating='post', value=0)
+    
+  model = BernoulliNB()
+  
+  clf = model.fit(x_train, y_train)
+  
+  predicted= clf.predict(x_test)
+
+  accuracy = metrics.accuracy_score(y_test, predicted)
+  print("BernoulliNB Accuracy:", accuracy)
   
 (x_train, y_train), (x_test, y_test) =  keras.datasets.imdb.load_data(num_words=None, maxlen=None)
 words_index = keras.datasets.imdb.get_word_index() #Gets word index where indexes are words
